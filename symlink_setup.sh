5fe07b129f98c7c59d32527f488d4dc0c07448e4
Roy Blankman <riblankman@gmail.com>
2013-11-27 18:32:39 +0200

    removing slate configs

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -2,5 +2,4 @@ ln -s ~/.dotfiles/vim           ~/.vim
 ln -s ~/.dotfiles/vimrc         ~/.vimrc
 ln -s ~/.dotfiles/gitconfig     ~/.gitconfig
 ln -s ~/.dotfiles/tmux.conf     ~/.tmux.conf
-ln -s ~/.dotfiles/slate         ~/.slate
 ln -s ~/.dotfiles/bash_profile  ~/.bash_profile
